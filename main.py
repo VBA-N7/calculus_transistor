12c2f3a58c4e11b84f818550927a1244824bb673
VBA-N7 <barthe.vincent@hotmail.fr>
2020-03-25 16:44:42 +0100

    Commit de rectification : J'ai oublié de stage tout les fichiers...

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# This Python file uses the following encoding: utf-8
+# main.py
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow
-
-
-class main(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = main()
+    app = QApplication(sys.argv)
+
+    file = QFile("mainwindow.ui")
+    file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(file)
     window.show()
+
     sys.exit(app.exec_())
